7c68dd4812c787f9c15a20b1603524abfdb36488
Matheus Vieira <matheus.fvp@outlook.com>
2024-04-20 21:06:35 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
         Department department = new Department();
         department.setId(1);
         department.setName("Department 1");
-        System.out.println(department);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(),
+                3000.0, department);
+        System.out.println(seller);
     }
 }
