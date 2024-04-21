f3035452d9399e3806b24dbb6a147b1dea20a75d
Matheus Vieira <matheus.fvp@outlook.com>
2024-04-20 21:17:29 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +15,9 @@ public class Program {
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(),
                 3000.0, department);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
     }
 }
